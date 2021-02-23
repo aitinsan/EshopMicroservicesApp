13c7f6de1ea2845b470686790802caf698c5aa2c
Insan Aitkozha <aitinsaninsan@gmail.com>
2021-02-23 20:31:04 +0600

    Добавьте файлы проекта.

---
+++ src/Ordering/Ordering.API/RabbitMQ/EventBusRabbitMQConsumer.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using EventBusRabbitMQ;
+using EventBusRabbitMQ.Common;
+using EventBusRabbitMQ.Events;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Ordering.API.RabbitMQ
+{
+    public class EventBusRabbitMQConsumer
+    {
+        private readonly IRabbitMQConnection _connection;
+        private readonly IMapper _mapper;
+        private IServiceScopeFactory _factory;
+
+        public EventBusRabbitMQConsumer(IRabbitMQConnection connection, IMapper mapper, IServiceScopeFactory factory)
+        {
+            _connection = connection;
+            _mapper = mapper;
+            _factory = factory;
+        }
+
+        public void Consume()
+        {
+            var channel = _connection.CreateModel();
+            channel.QueueDeclare(queue: EventBusConstants.BasketCheckoutQueue, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += OnReceivedEvent;
+
+            channel.BasicConsume(queue: EventBusConstants.BasketCheckoutQueue, autoAck: true, consumer: consumer);
+        }
+
+        private async void OnReceivedEvent(object sender, BasicDeliverEventArgs e)
+        {
+            if (e.RoutingKey == EventBusConstants.BasketCheckoutQueue)
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                var order = JsonConvert.DeserializeObject<Order>(message);
+                using var scope = _factory.CreateScope();
+                var repos = scope.ServiceProvider.GetService<IOrderRepository>();
+                await repos.AddAsync(order);
+            }
+        }
+
+        public void Disconnect()
+        {
+            _connection.Dispose();
+        }
+    }
+}
